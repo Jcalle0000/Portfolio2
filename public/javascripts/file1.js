3f80cfb2a654acd57da75a9bca59b891228014c0
Jason <jcalle0000@gmail.com>
2020-11-26 19:06:26 -0500

    font mod1

--- public/javascripts/file1.js
+++ public/javascripts/file1.js
@@ -4,11 +4,11 @@ const t1=gsap.timeline({
     defaults:{ease:"power1.out"}
 })
 
-t1.to('.text', {y:"0%", duration:1, stagger:0.25})
+// t1.to('.text', {y:"0%", duration:1, stagger:0.25})
 
-t1.to('.slider', {y:"-100%", duration:1.5, delay: 0.5 })
+// t1.to('.slider', {y:"-100%", duration:1.5, delay: 0.5 })
 
-t1.to('.intro', {y:"-100%", duration: 1 }, "-=1")
+// t1.to('.intro', {y:"-100%", duration: 1 }, "-=1")
 
 
 
