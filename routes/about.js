e67b0527b05c4198fc0a189f0ca2ac266b86adc0
Jason <jcalle0000@gmail.com>
2021-02-26 14:23:54 -0500

    content + background color

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,11 @@
 const router= require('express').Router()
 
+
 router.get('/', (req,res)=>{
     res.render('pages/aboutPage.ejs')
 })
 
+// localhost:3636/about/exp
 router.get('/exp', (req,res)=>{
     res.render('pages/experiment.ejs')
 } )
